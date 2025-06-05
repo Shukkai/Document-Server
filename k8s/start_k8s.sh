13ac002525f5caf0a33a7ef8a8e441ed1e2b234d
R-a-y-s <0410330@gmail.com>
2025-06-05 23:47:14 +0800

    k8s debug

--- k8s/start_k8s.sh
+++ k8s/start_k8s.sh
@@ -1,4 +1,7 @@
-k3d cluster create cn-doc --api-port 6666 -p "80:80@loadbalancer"
+k3d cluster create doccen --api-port 6666 -p "80:80@loadbalancer"
+kubectl create configmap prometheus-config --from-file=../prometheus/prometheus.yml
+kubectl create configmap grafana-datasource --from-file=../grafana/datasource.yaml
+kubectl create configmap grafana-dashboard  --from-file=../grafana/dashboard.yaml --from-file=../grafana/dashboards/
 kubectl apply -f deploys.yaml
 kubectl apply -f services.yaml
 kubectl apply -f ingress.yaml
